fa8ecbf519782afd55e399836b56425c78a6898b
vlad-ki <inweb24.vk@gmail.com>
2017-05-09 16:40:06 +0300

    deal

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script calculate nique words in text and print 10 most popular in it
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+ 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,49 @@
-
-
 def load_data(filepath):
-    pass
+    with open(filepath) as file_handler:
+        return file_handler.read()
+
+
+def text_replace(text):
+    text = text.replace('-\n', '')
+    text = text.replace('\n', ' ')
+    text = text.replace('"', '')
+    text = text.replace("'", "")
+    text = text.replace('.', '')
+    text = text.replace(',', '')
+    text = text.replace(':', '')
+    text = text.replace(';', '')
+    text = text.replace('- ', ' ')
+    text = text.replace('\u2018', '')
+    text = text.replace('\u2019', '')
+    text = text.replace('\u00AB', '')
+    text = text.replace('\u00BB', '')
+
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    text = text_replace(text)
+    words = {word for word in text.split(' ')}
+    words -= {''}
+    words_and_count = {}
+    top_10_words = {}
+    count = 0
+
+    for word in words:
+        word += ' '
+        words_and_count[word.strip()] = text.count(word)
+
+    for word in reversed(sorted(words_and_count, key=words_and_count.get)):
+        top_10_words[word] = words_and_count[word]
+        count += 1
+        if count == 10:
+            break
+
+    return top_10_words
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Введите путь к файлу: ')
+    top_10_words = get_most_frequent_words(load_data(filepath))
+    for word in top_10_words:
+        print('{}: {}\n'.format(word, top_10_words[word]))
