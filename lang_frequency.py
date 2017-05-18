b70ee88506e1c66ef923e336ee862107f6133e8d
vlad-ki <inweb24.vk@gmail.com>
2017-05-18 16:50:12 +0300

    regex for serch words and new logic of count

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,3 +1,7 @@
+import re
+from collections import Counter
+
+
 def load_data(filepath):
     with open(filepath) as file_handler:
         return file_handler.read()
@@ -6,44 +10,19 @@ def load_data(filepath):
 def text_replace(text):
     text = text.replace('-\n', '')
     text = text.replace('\n', ' ')
-    text = text.replace('"', '')
-    text = text.replace("'", "")
-    text = text.replace('.', '')
-    text = text.replace(',', '')
-    text = text.replace(':', '')
-    text = text.replace(';', '')
-    text = text.replace('- ', ' ')
-    text = text.replace('\u2018', '')
-    text = text.replace('\u2019', '')
-    text = text.replace('\u00AB', '')
-    text = text.replace('\u00BB', '')
+    r_ex = re.compile(r'[\w]{3,}')
+    words = r_ex.findall(text)
 
-    return text
+    return words
 
 
-def get_most_frequent_words(text):
-    text = text_replace(text)
-    words = {word for word in text.split(' ')}
-    words -= {''}
-    words_and_count = {}
-    top_10_words = {}
-    count = 0
-
-    for word in words:
-        word += ' '
-        words_and_count[word.strip()] = text.count(word)
-
-    for word in reversed(sorted(words_and_count, key=words_and_count.get)):
-        top_10_words[word] = words_and_count[word]
-        count += 1
-        if count == 10:
-            break
-
-    return top_10_words
+def get_most_frequent_words(text, count):
+    words = text_replace(text)
+    return Counter(words).most_common(count)
 
 
 if __name__ == '__main__':
     filepath = input('Введите путь к файлу: ')
-    top_10_words = get_most_frequent_words(load_data(filepath))
-    for word in top_10_words:
-        print('{}: {}\n'.format(word, top_10_words[word]))
+    top_10_words = get_most_frequent_words(load_data(filepath), 10)
+    for word, count in top_10_words:
+        print('{}: {}\n'.format(word, count))
